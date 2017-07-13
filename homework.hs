4a0f13b0c74ff4ef3eb7ea1fc8d08a43ea176bc6
syzer3@gmail.com <syzer3@gmail.com>
2017-07-13 17:45:42 +0200

    euler 91

--- homework.hs
+++ homework.hs
@@ -19,3 +19,18 @@ sum [1..10]
 --Find the sum of all the multiples of 3 or 5 below 1000.
 sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 --233168
+
+
+--euler91 (tringles)
+--a^2 + b^2 = c^2
+--https://projecteuler.net/problem=91
+--There are exactly fourteen triangles containing a right angle that can be formed when each co-ordinate lies between 0 and 2 inclusive; that is,
+--0 ≤ x1, y1, x2, y2 ≤ 2.
+
+--Given that 0 ≤ x1, y1, x2, y2 ≤ 50, how many right triangles can be formed?
+let triangles = [(a,b,c) | c <- [1..100], b <- [1..100], a <- [1..10]]
+let tianglesWithRightAngle = [(a,b,c) | c <- [1..50], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+-- [(3,4,5),(6,8,10)]
+euler91 = length tianglesWithRightAngle
+--2
+
