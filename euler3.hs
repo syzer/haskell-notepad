9049a98a79d659a9f24896851fbb5d75c6d15342
syzer3@gmail.com <syzer3@gmail.com>
2017-07-14 09:30:24 +0200

    curried functions

--- euler3.hs
+++ euler3.hs
@@ -1,2 +1,9 @@
 --euler 3
 import Math.NumberTheory.Primes.Factorisation
+
+
+--prime_factors n =
+--  case factors of
+--    [] -> [n]
+--    _  -> factors ++ prime_factors (n `div` (head factors))
+--  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
--- hello.hs
+++ hello.hs
@@ -1,1 +1,16 @@
 main = putStrLn "Hello, World!"
+
+--\x ->
+add :: (Int, Int) -> Int
+add (x,y) = x + y
+
+add 3 4
+
+-- add using lambda
+add2 = \(x, y) -> x+y
+
+--add3 :: Int -> Int -> Int
+add3 x y = x + y
+
+--curried
+add` = \x -> \y -> x + y
